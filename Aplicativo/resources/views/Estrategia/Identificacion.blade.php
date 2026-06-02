83abb7770cbcfa7878d385b0f89caccf8368d8c6
JesusEscalante <48699374+JesusEscalante@users.noreply.github.com>
2026-06-02 13:49:26 -0500

    Identificación de estrategias - diseño titulo

--- Aplicativo/resources/views/Estrategia/Identificacion.blade.php
+++ Aplicativo/resources/views/Estrategia/Identificacion.blade.php
@@ -9,8 +9,8 @@
 
             <div class="card shadow mb-4">
                 <div class="card-header py-3">
-                    <div class="row align-items-center">
-                        <div class="col-lg-6 col-sm-12"><h5 class="font-weight-bold text-primary m-0">IDENTIFICACIÓN DE ESTRATEGIAS </h5></div>
+                    <div class="align-items-center">
+                        <h5 class="font-weight-bold text-primary m-0">IDENTIFICACIÓN DE ESTRATEGIAS </h5>
                     </div>
                 </div>
 
--- Aplicativo/resources/views/Plan/ObjetivosEsp.blade.php
+++ Aplicativo/resources/views/Plan/ObjetivosEsp.blade.php
@@ -6,7 +6,7 @@
     <div class="card shadow mb-4">
         <div class="card-header py-3">
             <div class="row align-items-center">
-                <div class="col-lg-10 col-sm-12"><h5 class="font-weight-bold text-primary">OBJETIVOS ESPECÍFICOS</h5></div>
+                <div class="col-lg-10 col-sm-12"><h5 class="font-weight-bold text-primary">OBJETIVOS ESPECÍFICOS PARA EL PLAN ESTRATEGICO</h5></div>
                 <div class="col-lg-2 col-sm-12 d-flex justify-content-end row">
                     <a href="#" class="btn btn-primary btn-icon-split ml-1" data-toggle="modal" data-target="#AddObjetivo">
                         <i class="fa fa-plus"></i>
